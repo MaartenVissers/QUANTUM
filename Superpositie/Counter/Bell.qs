4e9b27a273b0a66c925d70f927817d0a805cb7fa
maartenvissers <maarten.vissers@student.kdg.be>
2019-02-06 16:25:26 +0100

    Superpositie & Entanglement poc's toegevoegd

---
+++ Superpositie/Counter/Bell.qs
@@ -0,0 +1,44 @@
+﻿namespace Quantum.Counter
+{
+	open Microsoft.Quantum.Primitive;
+	open Microsoft.Quantum.Canon;
+	
+	//Je geeft een qubit mee en je geeft mee wat je wilt dat hij wordt
+	//Wil je One (in desired) en heeft de qubit een waarde van Zero dan gaat het de qubit flippen.
+	//M (measure) kijkt of er een One of Zero in zit, vanaf je M oproept op een qubit kan het niet meer in superposition zitten.
+	operation Set (desired : Result, q1: Qubit) : Unit {
+		let current = M(q1);
+
+		if (desired != current){
+			X(q1);
+		}
+	}
+
+	//Elke keer dat we meten, vragen we om een klassieke waarde, maar de qubit ligt halverwege tussen 0 en 1, dus krijgen we (statistisch) 0 de helft van de tijd en 
+	//1 de helft van de tijd. Dit staat bekend als superpositie en geeft ons onze eerste reële blik in een quantumtoestand.
+	operation BellTest (count: Int, initial: Result) : (Int, Int)
+	{
+		mutable numOnes = 0;
+
+		using (qubit = Qubit()){
+			for (test in 1..count){
+				Set(initial, qubit);
+
+				//Hadamard gate 
+				//X flipt een qubit van 0 tot 1
+				//H flipt een qubit maar half
+				H(qubit);
+				let res = M(qubit);
+
+				if(res == One){
+					set numOnes = numOnes + 1;
+
+				}
+			}
+
+			Set(Zero, qubit);
+		}
+
+		return (count - numOnes, numOnes);
+	}
+}
---
+++ Superpositie/Counter/obj/qsharp/src/Bell.g.cs
@@ -0,0 +1,190 @@
+#pragma warning disable 1591
+using System;
+using Microsoft.Quantum.Core;
+using Microsoft.Quantum.Primitive;
+using Microsoft.Quantum.Simulation.Core;
+
+[assembly: Microsoft.Quantum.QsCompiler.Attributes.CallableDeclaration("{\"Kind\":{\"Case\":\"Operation\"},\"QualifiedName\":{\"Namespace\":\"Quantum.Counter\",\"Name\":\"Set\"},\"SourceFile\":\"E:/Maarten/Documents/School/Derde%20Jaar/TIConference/Counter/Counter/Bell.qs\",\"Position\":{\"Item1\":8,\"Item2\":1},\"SymbolRange\":{\"Item1\":{\"Line\":1,\"Column\":11},\"Item2\":{\"Line\":1,\"Column\":14}},\"ArgumentTuple\":{\"Case\":\"QsTuple\",\"Fields\":[[{\"Case\":\"QsTupleItem\",\"Fields\":[{\"VariableName\":{\"Case\":\"ValidName\",\"Fields\":[\"desired\"]},\"Type\":{\"Case\":\"Result\"},\"IsMutable\":false,\"HasLocalQuantumDependency\":false,\"Position\":{\"Case\":\"Null\"},\"Range\":{\"Item1\":{\"Line\":1,\"Column\":16},\"Item2\":{\"Line\":1,\"Column\":23}}}]},{\"Case\":\"QsTupleItem\",\"Fields\":[{\"VariableName\":{\"Case\":\"ValidName\",\"Fields\":[\"q1\"]},\"Type\":{\"Case\":\"Qubit\"},\"IsMutable\":false,\"HasLocalQuantumDependency\":false,\"Position\":{\"Case\":\"Null\"},\"Range\":{\"Item1\":{\"Line\":1,\"Column\":34},\"Item2\":{\"Line\":1,\"Column\":36}}}]}]]},\"Signature\":{\"TypeParameters\":[],\"ArgumentType\":{\"Case\":\"TupleType\",\"Fields\":[[{\"Case\":\"Result\"},{\"Case\":\"Qubit\"}]]},\"ReturnType\":{\"Case\":\"UnitType\"},\"SupportedFunctors\":[]},\"Documentation\":[]}")]
+[assembly: Microsoft.Quantum.QsCompiler.Attributes.SpecializationDeclaration("{\"Kind\":{\"Case\":\"QsBody\"},\"Parent\":{\"Namespace\":\"Quantum.Counter\",\"Name\":\"Set\"},\"SourceFile\":\"E:/Maarten/Documents/School/Derde%20Jaar/TIConference/Counter/Counter/Bell.qs\",\"Position\":{\"Item1\":8,\"Item2\":1},\"HeaderRange\":{\"Item1\":{\"Line\":1,\"Column\":11},\"Item2\":{\"Line\":1,\"Column\":14}},\"Documentation\":[]}")]
+[assembly: Microsoft.Quantum.QsCompiler.Attributes.CallableDeclaration("{\"Kind\":{\"Case\":\"Operation\"},\"QualifiedName\":{\"Namespace\":\"Quantum.Counter\",\"Name\":\"BellTest\"},\"SourceFile\":\"E:/Maarten/Documents/School/Derde%20Jaar/TIConference/Counter/Counter/Bell.qs\",\"Position\":{\"Item1\":17,\"Item2\":1},\"SymbolRange\":{\"Item1\":{\"Line\":1,\"Column\":11},\"Item2\":{\"Line\":1,\"Column\":19}},\"ArgumentTuple\":{\"Case\":\"QsTuple\",\"Fields\":[[{\"Case\":\"QsTupleItem\",\"Fields\":[{\"VariableName\":{\"Case\":\"ValidName\",\"Fields\":[\"count\"]},\"Type\":{\"Case\":\"Int\"},\"IsMutable\":false,\"HasLocalQuantumDependency\":false,\"Position\":{\"Case\":\"Null\"},\"Range\":{\"Item1\":{\"Line\":1,\"Column\":21},\"Item2\":{\"Line\":1,\"Column\":26}}}]},{\"Case\":\"QsTupleItem\",\"Fields\":[{\"VariableName\":{\"Case\":\"ValidName\",\"Fields\":[\"initial\"]},\"Type\":{\"Case\":\"Result\"},\"IsMutable\":false,\"HasLocalQuantumDependency\":false,\"Position\":{\"Case\":\"Null\"},\"Range\":{\"Item1\":{\"Line\":1,\"Column\":33},\"Item2\":{\"Line\":1,\"Column\":40}}}]}]]},\"Signature\":{\"TypeParameters\":[],\"ArgumentType\":{\"Case\":\"TupleType\",\"Fields\":[[{\"Case\":\"Int\"},{\"Case\":\"Result\"}]]},\"ReturnType\":{\"Case\":\"TupleType\",\"Fields\":[[{\"Case\":\"Int\"},{\"Case\":\"Int\"}]]},\"SupportedFunctors\":[]},\"Documentation\":[]}")]
+[assembly: Microsoft.Quantum.QsCompiler.Attributes.SpecializationDeclaration("{\"Kind\":{\"Case\":\"QsBody\"},\"Parent\":{\"Namespace\":\"Quantum.Counter\",\"Name\":\"BellTest\"},\"SourceFile\":\"E:/Maarten/Documents/School/Derde%20Jaar/TIConference/Counter/Counter/Bell.qs\",\"Position\":{\"Item1\":17,\"Item2\":1},\"HeaderRange\":{\"Item1\":{\"Line\":1,\"Column\":11},\"Item2\":{\"Line\":1,\"Column\":19}},\"Documentation\":[]}")]
+#line hidden
+namespace Quantum.Counter
+{
+    public class Set : Operation<(Result,Qubit), QVoid>, ICallable
+    {
+        public Set(IOperationFactory m) : base(m)
+        {
+        }
+
+        public class In : QTuple<(Result,Qubit)>, IApplyData
+        {
+            public In((Result,Qubit) data) : base(data)
+            {
+            }
+
+            System.Collections.Generic.IEnumerable<Qubit> IApplyData.Qubits
+            {
+                get
+                {
+                    yield return Data.Item2;
+                }
+            }
+        }
+
+        String ICallable.Name => "Set";
+        String ICallable.FullName => "Quantum.Counter.Set";
+        protected ICallable<Qubit, Result> M
+        {
+            get;
+            set;
+        }
+
+        protected IUnitary<Qubit> MicrosoftQuantumPrimitiveX
+        {
+            get;
+            set;
+        }
+
+        public override Func<(Result,Qubit), QVoid> Body => (__in__) =>
+        {
+            var (desired,q1) = __in__;
+#line 10 "E:\\Maarten\\Documents\\School\\Derde Jaar\\TIConference\\Counter\\Counter\\Bell.qs"
+            var current = M.Apply(q1);
+#line 12 "E:\\Maarten\\Documents\\School\\Derde Jaar\\TIConference\\Counter\\Counter\\Bell.qs"
+            if ((desired != current))
+            {
+#line 13 "E:\\Maarten\\Documents\\School\\Derde Jaar\\TIConference\\Counter\\Counter\\Bell.qs"
+                MicrosoftQuantumPrimitiveX.Apply(q1);
+            }
+
+#line hidden
+            return QVoid.Instance;
+        }
+
+        ;
+        public override void Init()
+        {
+            this.M = this.Factory.Get<ICallable<Qubit, Result>>(typeof(Microsoft.Quantum.Primitive.M));
+            this.MicrosoftQuantumPrimitiveX = this.Factory.Get<IUnitary<Qubit>>(typeof(Microsoft.Quantum.Primitive.X));
+        }
+
+        public override IApplyData __dataIn((Result,Qubit) data) => new In(data);
+        public override IApplyData __dataOut(QVoid data) => data;
+        public static System.Threading.Tasks.Task<QVoid> Run(IOperationFactory __m__, Result desired, Qubit q1)
+        {
+            return __m__.Run<Set, (Result,Qubit), QVoid>((desired, q1));
+        }
+    }
+
+    public class BellTest : Operation<(Int64,Result), (Int64,Int64)>, ICallable
+    {
+        public BellTest(IOperationFactory m) : base(m)
+        {
+        }
+
+        public class In : QTuple<(Int64,Result)>, IApplyData
+        {
+            public In((Int64,Result) data) : base(data)
+            {
+            }
+
+            System.Collections.Generic.IEnumerable<Qubit> IApplyData.Qubits => null;
+        }
+
+        public class Out : QTuple<(Int64,Int64)>, IApplyData
+        {
+            public Out((Int64,Int64) data) : base(data)
+            {
+            }
+
+            System.Collections.Generic.IEnumerable<Qubit> IApplyData.Qubits => null;
+        }
+
+        String ICallable.Name => "BellTest";
+        String ICallable.FullName => "Quantum.Counter.BellTest";
+        protected Allocate Allocate
+        {
+            get;
+            set;
+        }
+
+        protected IUnitary<Qubit> MicrosoftQuantumPrimitiveH
+        {
+            get;
+            set;
+        }
+
+        protected ICallable<Qubit, Result> M
+        {
+            get;
+            set;
+        }
+
+        protected Release Release
+        {
+            get;
+            set;
+        }
+
+        protected ICallable<(Result,Qubit), QVoid> Set
+        {
+            get;
+            set;
+        }
+
+        public override Func<(Int64,Result), (Int64,Int64)> Body => (__in__) =>
+        {
+            var (count,initial) = __in__;
+#line 20 "E:\\Maarten\\Documents\\School\\Derde Jaar\\TIConference\\Counter\\Counter\\Bell.qs"
+            var numOnes = 0L;
+#line hidden
+            {
+#line 22 "E:\\Maarten\\Documents\\School\\Derde Jaar\\TIConference\\Counter\\Counter\\Bell.qs"
+                var qubit = Allocate.Apply();
+#line 23 "E:\\Maarten\\Documents\\School\\Derde Jaar\\TIConference\\Counter\\Counter\\Bell.qs"
+                foreach (var test in new Range(1L, count))
+#line hidden
+                {
+#line 24 "E:\\Maarten\\Documents\\School\\Derde Jaar\\TIConference\\Counter\\Counter\\Bell.qs"
+                    Set.Apply((initial, qubit));
+#line 29 "E:\\Maarten\\Documents\\School\\Derde Jaar\\TIConference\\Counter\\Counter\\Bell.qs"
+                    MicrosoftQuantumPrimitiveH.Apply(qubit);
+#line 30 "E:\\Maarten\\Documents\\School\\Derde Jaar\\TIConference\\Counter\\Counter\\Bell.qs"
+                    var res = M.Apply(qubit);
+#line 32 "E:\\Maarten\\Documents\\School\\Derde Jaar\\TIConference\\Counter\\Counter\\Bell.qs"
+                    if ((res == Result.One))
+                    {
+#line 33 "E:\\Maarten\\Documents\\School\\Derde Jaar\\TIConference\\Counter\\Counter\\Bell.qs"
+                        numOnes = (numOnes + 1L);
+                    }
+                }
+
+#line 38 "E:\\Maarten\\Documents\\School\\Derde Jaar\\TIConference\\Counter\\Counter\\Bell.qs"
+                Set.Apply((Result.Zero, qubit));
+#line hidden
+                Release.Apply(qubit);
+            }
+
+#line 41 "E:\\Maarten\\Documents\\School\\Derde Jaar\\TIConference\\Counter\\Counter\\Bell.qs"
+            return ((count - numOnes), numOnes);
+        }
+
+        ;
+        public override void Init()
+        {
+            this.Allocate = this.Factory.Get<Allocate>(typeof(Microsoft.Quantum.Primitive.Allocate));
+            this.MicrosoftQuantumPrimitiveH = this.Factory.Get<IUnitary<Qubit>>(typeof(Microsoft.Quantum.Primitive.H));
+            this.M = this.Factory.Get<ICallable<Qubit, Result>>(typeof(Microsoft.Quantum.Primitive.M));
+            this.Release = this.Factory.Get<Release>(typeof(Microsoft.Quantum.Primitive.Release));
+            this.Set = this.Factory.Get<ICallable<(Result,Qubit), QVoid>>(typeof(Set));
+        }
+
+        public override IApplyData __dataIn((Int64,Result) data) => new In(data);
+        public override IApplyData __dataOut((Int64,Int64) data) => new Out(data);
+        public static System.Threading.Tasks.Task<(Int64,Int64)> Run(IOperationFactory __m__, Int64 count, Result initial)
+        {
+            return __m__.Run<BellTest, (Int64,Result), (Int64,Int64)>((count, initial));
+        }
+    }
+}
